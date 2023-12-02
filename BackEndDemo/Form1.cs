36a9c9e31a1b2d3f7abb3829cd7b6994f5ea7a1e
PriyanshuNiranjan <134698747+Priyanshuosdbms@users.noreply.github.com>
2023-12-02 20:11:52 +0530

    Add files via upload

---
+++ BackEndDemo/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace BackEndDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = "Data Source=DESKTOP-7KE8K8N\\SQLEXPRESS;Initial Catalog=priyanshu;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+
+                string firstName = textBox1.Text;
+                string secondName = textBox2.Text;  // Change the variable name here
+
+                string query = "INSERT INTO Names(FirstName, SecondName) VALUES (@FirstName, @SecondName)";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@FirstName", firstName);
+                    cmd.Parameters.AddWithValue("@SecondName", secondName);  // Change the parameter name here
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Data has been saved");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = "Data Source=DESKTOP-7KE8K8N\\SQLEXPRESS;Initial Catalog=priyanshu;Integrated Security=True";
+
+            using(SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+
+                string firstName = textBox1.Text;
+                string secondName = textBox2.Text;
+
+                string query = "DELETE FROM Names WHERE FirstName = @FirstName AND SecondName = @SecondName";
+
+
+                using (SqlCommand cmd = new SqlCommand(query,con))
+                {
+                    cmd.Parameters.AddWithValue("@FirstName", firstName); // delete's syntax
+                    cmd.Parameters.AddWithValue("@SecondName", secondName);
+
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+
+            MessageBox.Show("Data Has Been Deleted");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = "Data Source=DESKTOP-7KE8K8N\\SQLEXPRESS;Initial Catalog=priyanshu;Integrated Security=True";
+
+            using (SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+
+                string query = "SELECT *FROM Names SORT";
+
+                using (SqlCommand cmd = new SqlCommand(query,con))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            MessageBox.Show("Data has been Sorted");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string ConnectionString = "Data Source=DESKTOP-7KE8K8N\\SQLEXPRESS;Initial Catalog=priyanshu;Integrated Security=True";
+
+            using(SqlConnection con = new SqlConnection(ConnectionString))
+            {
+                con.Open();
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Program
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int count;
+            Console.WriteLine("Inside main");
+            List<Employee> users = new List<Employee>();
+            Console.WriteLine("Enter total entries");
+            count = Convert.ToInt32(Console.ReadLine());
+            citizen ciz = new citizen();
+            
+        }
+    }
+
+    public class Employee
+    {
+        List<string> name = new List<string>();
+        List<int> salary = new List<int>();
+
+        public void getdata()
+        {
+            String? user;
+            int sal;
+            Console.WriteLine("Enter name:");
+            user = Console.ReadLine();
+            Console.WriteLine("Enter salary");
+            sal = Convert.ToInt32(Console.ReadLine());
+            name.Append(user);
+            salary.Append(sal);        
+        }
+
+        public void display()
+        {
+            foreach (string num in name)
+            {
+                Console.WriteLine(num);
+            }
+        }
+
+        public void displayhighest(int count)
+        {
+            name.Sort();
+            Console.WriteLine("Highest Salary : " + name.ElementAt(count - 1));
+        }
+
+    }
+
+    public class citizen
+    {
+        public string name;
+        public int age;
+        public int mobile_number;
+
+        public citizen()
+        {
+            Console.WriteLine("Enter name, age, phone no.");
+            name = Console.ReadLine();
+            age = Convert.ToInt32(Console.ReadLine());
+            mobile_number = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                if (mobile_number.ToString().Length != 10)
+                {
+                    throw new InvalidMn("Error");
+                }
+
+                catch (InvalidMn ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            } 
+        }
+    }
+        
+}
